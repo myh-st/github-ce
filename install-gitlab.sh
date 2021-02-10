5f53ba2d4587ea8e2ef367a8c5fc75708f1c03f3
myh-st <37788058+myh-st@users.noreply.github.com>
2021-02-10 14:34:29 +0700

    Add files via upload

---
+++ install-gitlab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Please Type URL for gitlab"?
+read -p 'URL: ' GITURL
+sudo yum install -y curl policycoreutils-python openssh-server perl
+sudo systemctl enable sshd
+sudo systemctl start sshd
+
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo systemctl reload firewalld
+
+sudo yum install postfix
+sudo systemctl enable postfix
+sudo systemctl start postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+sudo EXTERNAL_URL="https://$GITURL" yum install -y gitlab-ee
